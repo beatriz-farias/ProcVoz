f0432b89db289aef13246e897061659beb4a218f
beatriz-farias <beatrizfarias.engcomp@gmail.com>
2025-06-10 00:08:38 -0300

    bugfix: Base API

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ const aiResponse = document.getElementById('ai-response');
 const storyText = document.getElementById('story-text');
 const errorMessage = document.getElementById('error-message');
 
-const API_BASE_URL = 'https://backend-darkmysteria.onrender.com/'; // Base URL para sua API
-const ASK_API_URL = `${API_BASE_URL}ask_ai_audio`;
-const INITIAL_RIDDLE_URL = `${API_BASE_URL}get_initial_riddle`;
+const API_BASE_URL = 'https://backend-darkmysteria.onrender.com'; // Base URL para sua API
+const ASK_API_URL = `${API_BASE_URL}/ask_ai_audio`;
+const INITIAL_RIDDLE_URL = `${API_BASE_URL}/get_initial_riddle`;
 
 // Variáveis de Gravação e Estado do Jogo
 let mediaRecorder;
